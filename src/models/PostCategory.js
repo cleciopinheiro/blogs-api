f07c4bdc55d01cc9525e631e5a483e57a1c24f5b
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-07-13 23:57:12 -0300

    Feat: Requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,35 @@
+/**
+*
+* @param {import('sequelize').Sequelize} sequelize
+* @param {import('sequelize').DataTypes} DataTypes
+*/
+
+const categoriesPostsAttributes = require('../attributes/categoriesPosts');
+
+const postCategoriesSchema = (sequelize, DataTypes) => {
+  const postCategoriesTable = sequelize.define('PostCategory', categoriesPostsAttributes,
+  {
+    tableName: 'PostCategories',
+    underscored: true,
+    timestamps: false,
+  });
+
+  postCategoriesTable.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: postCategoriesTable,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: postCategoriesTable,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return postCategoriesTable;
+};
+
+module.exports = postCategoriesSchema;
